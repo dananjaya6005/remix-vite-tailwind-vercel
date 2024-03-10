790f97ebdc20a865178e3a00c77ce256db0943cc
dananjaya6005 <dananjaya6005@gmail.com>
2024-03-10 20:19:19 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.tsx",
+    "./app/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
