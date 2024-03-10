8ed09b1ad61bdd35907fa915bf5006a2cd08f885
dananjaya6005 <dananjaya6005@gmail.com>
2024-03-10 20:34:14 +0530

    try1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./app/**/*.tsx",
-    "./app/**/*.css",
+    "./app/**/*.{ts,tsx}", "./app/**/*.{html,erb}",
   ],
   theme: {
     extend: {},
