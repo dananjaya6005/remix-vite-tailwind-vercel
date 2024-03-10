4314e6966fa1bb0d5e73f0372078d6d894394345
dananjaya6005 <dananjaya6005@gmail.com>
2024-03-10 20:59:49 +0530

    try2

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
-import "./tailwind.css?inline";
+import '../public/dist/tailwind.css';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
